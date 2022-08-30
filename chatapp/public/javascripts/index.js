374045a8d1259fc7aeea88bd79aa586b5fa52489
halfrim0-0 <fried-potate-4go@docomo.ne.jp>
2022-08-30 17:22:19 +0900

    ユーザ名の表示

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -3,9 +3,9 @@
 // チャットルームに入室する
 function enter() {
     // 入力されたユーザ名を取得する
-    const userName = '';
-    // ユーザ名が未入力でないかチェックする
+    const userName = $('#userName').val();
 
+    // ユーザ名が未入力でないかチェックする
 
     $('form').submit();
 }
