544ad18bfb129512cd10e542dd620306bbd29e0c
konekato <k1shock1125@gmail.com>
2022-09-06 18:23:35 +0900

    Fix: socket id 取得

--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -1,11 +1,22 @@
 'use strict';
 
-module.exports = function (socket, io) {
-    // 入室メッセージをクライアントに送信する
+module.exports = function (socket, io) { 
+    /*
+    入室メッセージをクライアントに送信する
+    引数 data = {
+        userName: <ユーザ名>,
+    }
+    */
     socket.on('enter', function (data) {
-        if (!data) {
+        if (!data.userName) {
             return;
         }
+
+        // socket ID 取得
+        const userId = socket.id;
+        const message = `${data.userName}（id: ${userId}）さんが入室しました`;
+        data.userId = userId;
+        data.message = message;
         socket.broadcast.emit('receiveEnterEvent', data);
     });
 };
