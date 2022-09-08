2ff1fa3691286bbb88d484f13943374722207722
Gamechi666 <max.daisuke@ezweb.ne.jp>
2022-09-08 20:40:24 +0900

    メモ機能にも投稿日時を追加

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -6,11 +6,24 @@ function memo() {
     const userName = $('#userName').val();
     // 入力されたメッセージを取得
     const message = $('#message').val();
+    //投稿日を取得
+    const date = new Date();
+    const nowDate = date.getFullYear()
+            + '/' + ('0' + (date.getMonth() + 1)).slice(-2)
+            + '/' + ('0' + date.getDate()).slice(-2)
+            + ' ' + ('0' + date.getHours()).slice(-2)
+            + ':' + ('0' + date.getMinutes()).slice(-2);
     // メモが入力されていない場合は投稿しない
     if (message && message.match(/\S/g)) {
         // メモの内容を投稿
-        const memo = userName + "さんのメモ:" + message;
-        $('#thread').prepend('<p>' + memo + '</p>');
+        $('#thread').prepend('<div>'
+        + '<p style="margin: 0;">'
+            + '<span class="my-msg" style="font-weight:700;">' + userName + 'さん:MEMO ' + '</span>'
+            + '<span style="color:grey;">' + nowDate + '</span>'
+        + '</p>'
+        + '<p>' + message + '</p>'
+    + '</div>');
+        
         $('#message').val("");
     }
 
