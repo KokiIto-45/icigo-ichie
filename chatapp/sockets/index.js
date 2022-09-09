60298bcf53c01ef876ef272fd5b8b2b54fc58d78
konekato <k1shock1125@gmail.com>
2022-09-09 15:58:13 +0900

    Add: Socket 切断時の処理

--- chatapp/sockets/index.js
+++ chatapp/sockets/index.js
@@ -16,5 +16,24 @@ module.exports = function (server) {
 
         // 退室モジュールの呼出
         require('./exit')(socket, io, onlineUsers);
+
+        // Socket 切断時の処理
+        socket.on('disconnect', () => {
+            // 切断された人の index 取得
+            const index = onlineUsers.findIndex(user => user.id === socket.id);
+            
+            // ユーザが存在する場合（例外ない限りこれ）
+            if (index >= 0) {
+                // ユーザ名取得
+                const userName = onlineUsers[index].name;
+
+                // onlineUsers から削除
+                onlineUsers.splice(index, 1);
+            
+                // 退室処理
+                socket.broadcast.emit('receiveExitRoomEvent', `${userName}さんが退室しました。`);
+                io.sockets.emit('deleteOnlineUsersEvent', onlineUsers);
+            }
+        });
     });
 };
